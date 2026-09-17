b73c09e7e2f498d59fb276b4c027a355aa4d55d6
Raphaël Bédard-Tremblay <rafi091107@gmail.com>
2026-09-17 14:42:25 -0400

    Add section pages and navbar links
    This commit creates the About, Contact, Team, Events, and Partners pages with Bootstrap layouts and consistent page metadata. It also updates the shared header navigation to point to the main site pages and removes old duplicated JS page files that were no longer used.

--- contact.php
+++ contact.php
@@ -1,22 +1,20 @@
-<!doctype html>
+  <!doctype html>
 <html lang="en">
-
-<head>
+  <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title> Contact </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-</head>
-
-<body>
+  </head>
+  <body>
     <header>
-        <?php require_once __DIR__ . "/inc/header.inc.php"; ?>
+      <?php require_once __DIR__ . "/inc/header.inc.php"; ?>
     </header>
     <main>
-        
+      <h1>Contact</h1>
+      <p>Ceci est une page d'exemple pour la section "Contact".</p>
+      <img src="path/to/image.jpg" alt="Description de l'image" class="img-fluid">
+      <p>Vous pouvez ajouter plus de contenu ici pour décrire votre site ou votre entreprise.</p>
     </main>
-    <script src="js/bootstrap.bundle.min.js"></script>
-</body>
-
+  </body>
 </html>
--- inc/header.inc.php
+++ inc/header.inc.php
@@ -1,31 +1,31 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">LOGO</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link" href="index.php">Acceuil</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
+          <a class="nav-link" href="apropos.php">À propos</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
+          <a class="nav-link" href="contact.php">Contact</a>
         </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown link
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
+        <li class="nav-item">
+          <a class="nav-link" href="equipe.php">Équipe</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="evenement.php">Événements</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="partenaire.php">Partenaires</a>
         </li>
       </ul>
+
     </div>
   </div>
 </nav>
