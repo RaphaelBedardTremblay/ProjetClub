5c2eae22ac54435af20e55a2485465f450945594
JustinMacfadyen <macfadyenj2016@gmail.com>
2026-09-17 14:12:39 -0400

    Hello World

--- index.view.php
+++ index.view.php
@@ -6,11 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
   </head>
   <body>
     <header>
       <?php require_once __DIR__ . "/inc/header.inc.php"; ?>
     </header>
+    <main>
+        <h1 class="">Hello World</h1>
+    </main>
+    <script src="js/bootstrap.bundle.min.js"></script>
   </body>
 </html>
   
